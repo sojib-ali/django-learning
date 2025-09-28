a512ba1cad557da36a8328b0dfb1fd0448b88a28
sojib-ali <sojib.ali@hotmail.com>
2025-09-28 17:16:32 +0600

    filtered through db

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 # Create your views here.
 def say_hello(request):
 
-    first_product = Product.objects.get(pk = 1)
+    # first_product = Product.objects.get(pk = 1)
     
-    #instead of try catch
-    product = Product.objects.get(pk = 0).first()
+    # #instead of try catch
+    # product = Product.objects.get(pk = 0).first()
 
-    #to know if a product exists - return a boolean
-    exists = Product.objects.filter(pk=0).exists()
+    # #to know if a product exists - return a boolean
+    # exists = Product.objects.filter(pk=0).exists()
+
+    product = Product.objects.filter(inventory__lt=10)
+
+    customer = Customer.objects.filter(id = 1)
+
+    collection = Collection.objects.filter(featured_product__isnull = True)
+
+    order = Order.objects.filter(customer__id=1)
+
+    order_item = OrderItem.objects.filter(product__collection__id=3)
 
 
-    return render(request, 'hello.html', {'name': 'sojib'})
+    return render(request, 'hello.html', {'name': 'sojib', 'products' : product, 'customers' : customer, 'orders': order}, )
