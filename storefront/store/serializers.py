9200866449b1ecd7c38e59e6a6d2f6aa24e858c2
sojib-ali <sojib.ali@hotmail.com>
2025-10-02 21:48:26 +0600

    got relational filed through primary key

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,11 +1,18 @@
 from decimal import Decimal
+from store.models import Product, Collection
 from rest_framework import serializers
+
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField
     title = serializers.CharField(max_length = 255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    #getting relational field through primary key
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset = Collection.objects.all()
+    )
 
     def calculate_tax(self, product):
         return product.unit_price * Decimal(1.1)
