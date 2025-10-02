bc22f549138b35efb8177f15144c72d42ad46bc4
sojib-ali <sojib.ali@hotmail.com>
2025-10-02 21:58:52 +0600

    get related field through nested object

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -3,14 +3,18 @@ from store.models import Product, Collection
 from rest_framework import serializers
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length = 255)
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField
     title = serializers.CharField(max_length = 255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source = 'unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
-    #getting relational field through primary key
-    collection = serializers.StringRelatedField()
+    #getting relational field through nested object
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product):
         return product.unit_price * Decimal(1.1)
