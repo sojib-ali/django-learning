882b96d84096b596f00c3a56fa8d353975e2ca6a
sojib-ali <sojib.ali@hotmail.com>
2025-10-02 20:23:03 +0600

    started restful api with simple get method

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -5,7 +5,8 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('store/', include('store.urls')),
 ] + debug_toolbar_urls()
 
 
