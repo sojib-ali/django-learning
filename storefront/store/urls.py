7e7af14351ec291382d841da4ea1dd15a81c8d06
sojib-ali <sojib.ali@hotmail.com>
2025-10-07 18:05:30 +0600

    converted it into viewsets

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.collection_detail, name = 'collection-detail') 
-]
+    path('', include(router.urls))
+] 
