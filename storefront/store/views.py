712cbe27f4b97a4904ab7a7d9cbc4cea75b21a15
sojib-ali <sojib.ali@hotmail.com>
2025-10-07 22:13:44 +0600

    filter through django-filter

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -11,16 +12,12 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['collection_id', 'unit_price']
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        
-        return queryset
+    
 
     def get_serializer_context(self):
         return {'request': self.request}
