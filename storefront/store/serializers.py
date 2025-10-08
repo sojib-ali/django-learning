107ae6ed511025d0a51fa7e18a761e8d4d83fab4
sojib-ali <sojib.ali@hotmail.com>
2025-10-08 21:12:07 +0600

    calculated total price on cart

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -48,10 +48,14 @@ class CartItemSerializer(serializers.ModelSerializer):
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many = True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
 
     class Meta:
         model = Cart
-        fields = ['id', 'items']
+        fields = ['id', 'items', 'total_price']
 
 
 
