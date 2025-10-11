1d0bad83e144144c5830db31394a99cb8b9b1a62
sojib-ali <sojib.ali@hotmail.com>
2025-10-12 00:24:04 +0600

    added customer with respect to user

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -20,11 +20,29 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
     list_per_page = 10
+
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (
+            reverse('admin:store_order_changelist')
+            + '?'
+            + urlencode({
+                'customer__id': str(customer.id)
+            }))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator
 from uuid import uuid4
@@ -46,18 +48,24 @@ class Customer(models.Model):
         Silver = 'S', 'Silver'
         Gold = 'G', 'Gold'
 
-    first_name = models.CharField( max_length=50)
-    last_name = models.CharField( max_length=50)
-    email = models.EmailField( max_length=100, unique=True)
     phone = models.CharField(max_length=20)
-    birth_date = models.DateField(null=True)
+    birth_date = models.DateField(null=True, blank = True)
     membership = models.CharField(max_length=1, choices=Membership, default=Membership.Bronze)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    @admin.display(ordering = 'user__first_name')
+    def first_name(self):
+        return self.user.first_name
+    
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
     
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 
