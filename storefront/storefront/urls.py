8246109cedf1bebe2a2e871b845ca065abd9f50a
sojib-ali <sojib.ali@hotmail.com>
2025-10-20 21:01:10 +0600

    setup media folder paths in the stetting MEDIA_ROOT

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -1,4 +1,6 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -10,6 +12,9 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 admin.site.site_header = 'Storefront admin'
